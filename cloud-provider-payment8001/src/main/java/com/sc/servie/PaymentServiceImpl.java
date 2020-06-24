e65a184423a44899dff3403d6e5738d9b9c05c50
Jokerboozp <2929590482@qq.com>
2020-06-24 15:52:32 +0800

    20200624-14-微服务测试

---
+++ cloud-provider-payment8001/src/main/java/com/sc/servie/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.sc.servie;
+
+import com.sc.entity.Payment;
+import com.sc.mapper.PaymentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService{
+
+    @Resource
+    private PaymentMapper paymentMapper;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentMapper.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentMapper.getPaymentById(id);
+    }
+}
