a08ae8cb1b65f13c91236ca6312cf3c2c40a3b2a
Jokerboozp <2929590482@qq.com>
2020-07-10 14:24:24 +0800

    20200710-79-Config完毕

--- cloud-config-client-3355/src/main/java/com/sc/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/sc/controller/ConfigClientController.java
@@ -1,10 +1,12 @@
 package com.sc.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${info}")   //此处是远程仓库的info
