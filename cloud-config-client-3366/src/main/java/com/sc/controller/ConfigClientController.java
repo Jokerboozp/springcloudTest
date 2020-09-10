eab3ad9d25f76c69573ed0840982c6adf240fd19
Jokerboozp <2929590482@qq.com>
2020-09-10 14:38:44 +0800

    20200910-81-3366创建并修改

--- cloud-config-client-3366/src/main/java/com/sc/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/sc/controller/ConfigClientController.java
@@ -4,19 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RefreshScope
 public class ConfigClientController {
 
     @Value("${server.port}")
     private String serverPort;
 
-    @Value("${config.info}")
+    @Value("${info}")
     private String configInfo;
 
     @GetMapping("/configInfo")
     public String configInfo(){
-        return "serverPort:"+serverPort+"configInfo:"+configInfo;
+        return configInfo;
     }
 }
